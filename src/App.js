733b9000f0bb8f12769529226554634bd6334969
laurenratcliffe <ratcliffelau@gmail.com>
2023-08-08 12:08:22 -0400

    made api call

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  useEffect (() => {
+    axios({
+      url: "https://api.edamam.com/api/recipes/v2?type=public&app_id=f9d938b5&app_key=9eda799447f92c7c378ad72894d88028%20%09&health=gluten-free&mealType=Dinner",
+      method: "GET",
+      dataResponse: "json",
+      params: { 
+        type: "public",
+        app_id: "f9d938b5",
+        app_key: "9eda799447f92c7c378ad72894d88028",
+        mealType: "dinner",
+        health: "gluten-free",
+      }
+    }).then((res) => { 
+      console.log(res);
+    })
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      
+    </>
   );
 }
 
